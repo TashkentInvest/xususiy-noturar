0e078e4d802120a10ea08a1389aeda9c24066c0d
InvestUz <invest.abdusattorov@gmail.com>
2025-04-01 14:08:45 +0500

    fixed

--- resources/views/pages/yertola/create.blade.php
+++ resources/views/pages/yertola/create.blade.php
@@ -180,7 +180,7 @@
                     <!-- Агар фойдаланиш мумкин бўлса -->
                     <div id="useFields" class="mt-4" style="display: none;">
                         <input type="number" name="umumiy_maydoni_yer_tola"
-                            class="form-control form-control-lg shadow-sm mb-2" placeholder="📏 Умумий ер майдони (м²)"
+                            class="form-control form-control-lg shadow-sm mb-2" placeholder="📏 Ертўланинг умумий майдони (м²)"
                             value="{{ old('ijaraga_berilgan_qismi_yer_tola') }}">
                         <input type="number" name="ijaraga_berilgan_qismi_yer_tola"
                             class="form-control form-control-lg shadow-sm mb-2"
--- resources/views/pages/yertola/index.blade.php
+++ resources/views/pages/yertola/index.blade.php
@@ -297,7 +297,7 @@
                                                 <table class="table table-bordered table-striped table-sm mb-0">
                                                     <tbody>
                                                         <tr>
-                                                            <td class="text-muted" width="60%">Умумий ер майдони
+                                                            <td class="text-muted" width="60%">Ертўланинг умумий майдони
                                                             </td>
                                                             <td class="fw-medium text-end">
                                                                 {{ $yertola->umumiy_maydoni_yer_tola ?? 'Маълумот йўқ' }}
